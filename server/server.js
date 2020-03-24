265d43f75e8fc0bfed21386efdeee1633c678148
Chris Bianca <chris@t0day.com>
2020-03-24 08:16:50 +0000

    Add random error generator to server responses

--- server/server.js
+++ server/server.js
@@ -37,62 +37,80 @@ const productsAsArray = Object.keys(productsLookup).map(
   key => productsLookup[key]
 );
 
+const isError = (errorPercentage = 10) => {
+  const rand = Math.random() * 100;
+  return rand <= errorPercentage;
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get("/api/products", (req, res) => {
   setTimeout(() => {
-    res.send({ products: productsAsArray });
+    if (isError()) {
+      res.status(500).send({ error: "Unexpected server error" });
+    } else {
+      res.send({ products: productsAsArray });
+    }
   }, 500);
 });
 
 app.get("/api/products/:id", (req, res) => {
   setTimeout(() => {
-    const id = req.params.id;
-    if (!id && id !== 0) {
-      res.status(400).send({ error: "No ID provided" });
-      return;
+    if (isError()) {
+      res.status(500).send({ error: "Unexpected server error" });
+    } else {
+      const id = req.params.id;
+      if (!id && id !== 0) {
+        res.status(400).send({ error: "No ID provided" });
+        return;
+      }
+      const product = productsLookup[req.params.id];
+      if (!product) {
+        res.status(404).send({ error: "No product found with that ID" });
+        return;
+      }
+      res.send(product);
     }
-    const product = productsLookup[req.params.id];
-    if (!product) {
-      res.status(404).send({ error: "No product found with that ID" });
-      return;
-    }
-    res.send(product);
   }, 500);
 });
 
 app.post("/api/checkout", (req, res) => {
   setTimeout(() => {
-    console.log(req.body);
-    const basket = req.body.products;
-    if (!basket) {
-      res.status(400).send({ error: "Invalid basket: No `products` field" });
-      return;
+    if (isError()) {
+      res.status(500).send({ error: "Unexpected server error" });
+    } else {
+      console.log(req.body);
+      const basket = req.body.products;
+      if (!basket) {
+        res.status(400).send({ error: "Invalid basket: No `products` field" });
+        return;
+      }
+      if (!Array.isArray(basket)) {
+        res.status(400).send({
+          error:
+            "Invalid basket: `products` field should be an array of product IDs"
+        });
+        return;
+      }
+      if (!basket.length) {
+        res.status(400).send({
+          error:
+            "Invalid basket: basket is empty (no product IDs were included)"
+        });
+        return;
+      }
+      const invalidProductIds = basket.filter(id => !(id in productsLookup));
+      if (invalidProductIds.length) {
+        res.status(400).send({
+          error: "Invalid basket: some product IDs were not found",
+          invalidProductIds
+        });
+        return;
+      }
+      res.send({ orderNumber: currentOrderNumber++ });
     }
-    if (!Array.isArray(basket)) {
-      res.status(400).send({
-        error:
-          "Invalid basket: `products` field should be an array of product IDs"
-      });
-      return;
-    }
-    if (!basket.length) {
-      res.status(400).send({
-        error: "Invalid basket: basket is empty (no product IDs were included)"
-      });
-      return;
-    }
-    const invalidProductIds = basket.filter(id => !(id in productsLookup));
-    if (invalidProductIds.length) {
-      res.status(400).send({
-        error: "Invalid basket: some product IDs were not found",
-        invalidProductIds
-      });
-      return;
-    }
-    res.send({ orderNumber: currentOrderNumber++ });
   }, 1000);
 });
 
